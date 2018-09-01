3a13e482e775457e3b602f3c36ae36e5844190cd
Lion994 <31659950+Lion994@users.noreply.github.com>
2018-09-01 10:27:21 +0200

    ajout class test

---
+++ testgithub/test.swift
@@ -0,0 +1,15 @@
+//
+//  test.swift
+//  testgithub
+//
+//  Created by Bruno Maunac on 01/09/2018.
+//  Copyright © 2018 Lion994. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut() {
+        print("salut")
+    }
+}
