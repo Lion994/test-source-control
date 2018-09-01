c81b0354c7c72fa3f1a40cdf0a2c2a4b0b6bad03
Lion994 <31659950+Lion994@users.noreply.github.com>
2018-09-01 10:19:32 +0200

    revert 2 commit

--- testgithub/ViewController.swift
+++ testgithub/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("2eme commit")
+        print("premier commit")
     }
 
     override func didReceiveMemoryWarning() {
